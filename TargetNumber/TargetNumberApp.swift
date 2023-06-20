48b9cddfe0f3984d5a3c509d3fbf43ba197a67ca
aleksei <alikexie93@yandex.ru>
2023-06-20 20:10:55 +0600

    Initial Commit

---
+++ TargetNumber/TargetNumberApp.swift
@@ -0,0 +1,17 @@
+//
+//  TargetNumberApp.swift
+//  TargetNumber
+//
+//  Created by Алексей Филиппов on 20.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct TargetNumberApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
